87ed3cadaa3a9cdfd9379cf7085301fad12a485b
Chen Tan <taydycn@gmail.com>
2019-05-23 13:38:05 +0800

    feat: 添加第 5 题 --> 最长回文子串

--- medium/5.go
+++ medium/5.go
@@ -1,23 +1,49 @@
 package medium
 
-// TODO has bug
+// https://leetcode-cn.com/problems/longest-palindromic-substring/
+// 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+//
+// 示例 1：
+//	 输入: "babad"
+//	 输出: "bab"
+//	 注意: "aba" 也是一个有效答案。
+//
+// 示例 2：
+//   输入: "cbbd"
+//   输出: "bb"
+//
+//
+// 思路:
+//	中心扩展算法:
+//		回文中心的两侧互为镜像。因此，回文可以从他的中心展开，并且只有2n-1个这样的中心(一个元素为中心的情况有n个，两个元素为中心的情况有n-1个)
+//
 func longestPalindrome(s string) string {
-	length := len(s)
-	chars := []rune(s)
-	max := 0
-	index := make([]int, 128, 128)
-	start := 0
-	for end := 0; end < length; end++ {
-
-		value := index[chars[end]]
-		if value > 0 {
-			if curLen := end - value + 1; curLen > max {
-				max = curLen
-				start = value - 1
-			}
-		}
-		index[chars[end]] = end + 1
+	if s == "" {
+		return ""
 	}
 
-	return string(chars[start : start+max+1])
+	chars := []rune(s)
+	var start, end int
+
+	for i := 0; i < len(s); i++ {
+		length := expandAroundCenter(chars, i, i)
+		lengthB := expandAroundCenter(chars, i, i+1)
+		if length < lengthB {
+			length = lengthB
+		}
+		if length > end-start {
+			start = i - (length-1)/2
+			end = i + length/2
+		}
+	}
+	return string(chars[start : end+1])
+}
+
+func expandAroundCenter(chars []rune, left, right int) int {
+	length := len(chars)
+	for ; left >= 0 && right < length && chars[left] == chars[right]; {
+		left--
+		right++
+	}
+	return right - left - 1
 }
--- medium/5_test.go
+++ medium/5_test.go
@@ -6,15 +6,24 @@ import (
 )
 
 func TestLongestPalindrome(t *testing.T) {
+
 	str := "babab"
-	if result := longestPalindrome(str); result != "bab" {
-		fmt.Printf("input %s, expect %s, actual %s \n", str, "bab", result)
+	if result := longestPalindrome(str); result != "babab" {
+		fmt.Printf("input %s, expect %s, actual %s \n", str, "babab", result)
 		t.FailNow()
 	}
 
 	str = "cbbd"
 	if result := longestPalindrome(str); result != "bb" {
 		fmt.Printf("input %s, expect %s, actual %s \n", str, "bb", result)
+		t.FailNow()
+	}
+
+	str = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabcaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+	fmt.Print(len(str))
+	except := "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+	if result := longestPalindrome(str); result != except {
+		fmt.Printf("input %s, expect %s, actual %s \n", str, except, result)
 		t.FailNow()
 	}
 }
