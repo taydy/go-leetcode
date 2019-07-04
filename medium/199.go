9b2bb2b81275a60613100e8df39ea2d51a35b060
Chen Tan <taydycn@gmail.com>
2019-07-04 18:05:58 +0800

    feat: 添加第 199 题 --> 二叉树的右视图

---
+++ medium/199.go
@@ -0,0 +1,82 @@
+package medium
+
+import (
+	"container/list"
+)
+
+// https://leetcode-cn.com/problems/binary-tree-right-side-view/
+// 给定一棵二叉树，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。
+// 示例:
+//
+//		输入: [1,2,3,null,5,null,4]
+//		输出: [1, 3, 4]
+// 解释:
+//
+//   		1            <---
+// 		  /   \
+//		 2     3         <---
+// 		 \     \
+//  	 5     4       	 <---
+//
+// 执行结果:
+// 执行用时 : 0 ms , 在所有 Go 提交中击败了 100.00% 的用户
+// 内存消耗 : 2.4 MB , 在所有 Go 提交中击败了 20.00% 的用户
+//
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// 递归
+func rightSideView(root *TreeNode) []int {
+	view := make([]int, 0)
+
+	return helper(root, 0, view)
+}
+
+func helper(root *TreeNode, level int, view []int) []int {
+	if root == nil {
+		return view
+	}
+	if len(view) == level {
+		view = append(view, root.Val)
+	}
+	view = helper(root.Right, level+1, view)
+	view = helper(root.Left, level+1, view)
+	return view
+}
+
+// 非递归
+func rightSideView2(root *TreeNode) []int {
+	view := make([]int, 0)
+
+	if root == nil {
+		return view
+	}
+
+	queue := list.New()
+	queue.PushFront(root)
+	for ; queue.Len() != 0; {
+		size := queue.Len()
+		for ; size > 0;  {
+			node := queue.Front()
+			val := node.Value.(*TreeNode).Val
+			queue.Remove(node)
+			if left := node.Value.(*TreeNode).Left; left != nil {
+				queue.PushBack(left)
+			}
+			if right := node.Value.(*TreeNode).Right; right != nil {
+				queue.PushBack(right)
+			}
+
+			size = size - 1
+			if size == 0 {
+				view = append(view, val)
+			}
+		}
+	}
+
+	return view
+}
---
+++ medium/199_test.go
@@ -0,0 +1,19 @@
+package medium
+
+import (
+	"testing"
+)
+
+func TestRightSideView(t *testing.T) {
+	root := &TreeNode{Val:1}
+	root.Left = &TreeNode{Val:2, Right:&TreeNode{Val:5}}
+	root.Right = &TreeNode{Val:3, Right:&TreeNode{Val:4}}
+
+	if views := rightSideView1(root); !equal(views, []int{1,3,4}) {
+		t.FailNow()
+	}
+
+	if views := rightSideView(nil); !equal(views, []int{}) {
+		t.FailNow()
+	}
+}
