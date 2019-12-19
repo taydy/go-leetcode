f6d69a7751ef5d084f6c53c7d9223d6e1dc3ccfe
部落格上de问候 <taydycn@gmail.com>
2019-12-19 16:29:16 +0800

    feat: 提交链表题目 21 合并两个有序链表

---
+++ easy/21.go
@@ -0,0 +1,27 @@
+package easy
+
+// 将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+//
+//示例：
+//
+//	输入：1->2->4, 1->3->4
+//	输出：1->1->2->3->4->4
+
+func MergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+
+	var result *ListNode
+	if l1.Val <= l2.Val {
+		result = l1
+		result.Next = MergeTwoLists(l1.Next, l2)
+	} else {
+		result = l2
+		result.Next = MergeTwoLists(l1, l2.Next)
+	}
+	return result
+}
---
+++ easy/21_test.go
@@ -0,0 +1,26 @@
+package easy
+
+import "testing"
+
+func TestMergeTwoLists(t *testing.T) {
+	l1 := &ListNode{
+		Val: 1,
+		Next: &ListNode{
+			Val: 2,
+			Next: &ListNode{
+				Val: 4,
+			},
+		},
+	}
+	l2 := &ListNode{
+		Val: 1,
+		Next: &ListNode{
+			Val: 3,
+			Next: &ListNode{
+				Val: 4,
+			},
+		},
+	}
+	result := MergeTwoLists(l1, l2)
+	t.Log(result)
+}
