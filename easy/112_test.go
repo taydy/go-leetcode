8c255edbf462e78ba99c0654e951a685646bb64d
部落格上de问候 <taydycn@gmail.com>
2019-12-25 18:36:35 +0800

    feat: 提交题目 112. 路径总和

---
+++ easy/112_test.go
@@ -0,0 +1,40 @@
+package easy
+
+import "testing"
+
+func TestHasPathSum(t *testing.T) {
+	root := &TreeNode{
+		Val: 5,
+		Left: &TreeNode{
+			Val: 4,
+			Left: &TreeNode{
+				Val: 11,
+				Left: &TreeNode{
+					Val: 7,
+				},
+				Right: &TreeNode{
+					Val: 2,
+				},
+			},
+		},
+		Right: &TreeNode{
+			Val: 8,
+			Left: &TreeNode{
+				Val: 13,
+			},
+			Right: &TreeNode{
+				Val: 4,
+				Right: &TreeNode{
+					Val: 1,
+				},
+			},
+		},
+	}
+
+	if !HasPathSum(root, 22) {
+		t.FailNow()
+	}
+	if HasPathSum(root, 21) {
+		t.FailNow()
+	}
+}
