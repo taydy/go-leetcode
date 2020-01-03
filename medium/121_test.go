6c307dea9f29a3dd0dd92d2f00bdf47c1cf15825
部落格上de问候 <taydycn@gmail.com>
2020-01-03 18:59:04 +0800

    feat: 条件题目 121. 买卖股票的最佳时机

---
+++ medium/121_test.go
@@ -0,0 +1,22 @@
+package medium
+
+import "testing"
+
+func TestMaxProfit(t *testing.T) {
+	inputs := [][]int{
+		{7, 1, 5, 3, 6, 4},
+		{7, 6, 4, 3, 1},
+	}
+	excepts := []int{
+		5, 0,
+	}
+
+	for i, v := range inputs {
+		result := MaxProfit(v)
+		if result != excepts[i] {
+			t.Logf("except %d, actual %d", excepts[i], result)
+			t.FailNow()
+		}
+	}
+
+}
