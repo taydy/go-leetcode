29498f928d44e2ce3ae4b348b1228505bee48b30
taydy <tanchen@bytedance.com>
2020-02-24 21:12:52 +0800

    feat: 提交题目 739. 每日温度

--- medium/739_test.go
+++ medium/739_test.go
@@ -4,5 +4,9 @@ import "testing"
 
 func TestDailyTemperatures(t *testing.T) {
 	temperatures := []int{73, 74, 75, 71, 69, 72, 76, 73}
-	t.Logf("%v", DailyTemperatures(temperatures))
+	except := []int{1, 1, 4, 2, 1, 1, 0, 0}
+	if result := DailyTemperatures(temperatures); !Equal(result, except) {
+		t.Logf("input temperatures %v, except output %v, but actual %v", temperatures, except, result)
+		t.FailNow()
+	}
 }
